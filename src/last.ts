a34a524951f2992b966ff9979e2fd87c5122d435
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-03-17 13:25:53 +0000

    refactor: use the TypeScript 4.x solution

--- src/last.ts
+++ src/last.ts
@@ -1,12 +1,4 @@
-import { Head } from './head'
-import { Tail } from './tail'
-
-export type Last<T extends unknown[]> = {
-  0: Head<T>
-  1: Last<Tail<T>>
-}[HasTail<T> extends true ? 1 : 0]
-
-type HasTail<T extends unknown[]> =
-  T extends [] | [unknown]
-  ? false
-  : true
+export type Last<T extends unknown[]> =
+  T extends [...unknown[], infer U]
+  ? U
+  : never
