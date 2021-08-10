f2dc258e04bbac0a5bc23182cd0da3942d2168e2
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-08-10 08:13:59 +0000

    refactor: improve return type

--- __tests__/replace-props-by-type.spec.ts
+++ __tests__/replace-props-by-type.spec.ts
@@ -5,24 +5,28 @@ describe('ReplacePropsByType<T, OldType, NewType>', () => {
   test('type', () => {
     interface Interface {
       type: string | number
+      other: Symbol
     }
 
     type Result = ReplacePropsByType<Interface, string, boolean>
 
     assertType<Equals<Result, {
       type: boolean | number
+    , other: Symbol
     }>>()
   })
 
   test('union type', () => {
     interface Interface {
       type: string | number | boolean
+      other: Symbol
     }
 
     type Result = ReplacePropsByType<Interface, string | number, RegExp>
 
     assertType<Equals<Result, {
       type: RegExp | boolean
+      other: Symbol
     }>>()
   })
 })
--- src/replace-props-by-type.ts
+++ src/replace-props-by-type.ts
@@ -1,5 +1,9 @@
 import { ReplaceType } from './replace-type'
+import { KeysExtendType } from './keys-extend-type'
 
 export type ReplacePropsByType<T, OldType, NewType> = {
-  [Key in keyof T]: ReplaceType<T[Key], OldType, NewType>
+  [Key in keyof T]: 
+    Key extends KeysExtendType<T, OldType>
+    ? ReplaceType<T[Key], OldType, NewType>
+    : T[Key]
 }
