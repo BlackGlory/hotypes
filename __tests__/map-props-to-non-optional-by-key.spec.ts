a298c6d437be6ac4f4bbda4e0802aecec7483fc4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-02-15 03:44:10 +0000

    test: add cases

--- __tests__/map-props-to-non-optional-by-key.spec.ts
+++ __tests__/map-props-to-non-optional-by-key.spec.ts
@@ -1,28 +1,56 @@
 import { assertType, Equals } from '@test/utils'
 import { MapPropsToNonOptionalByKey } from '@src/map-props-to-non-optional-by-key'
 
-test('MapPropsToNonOptionalByKey<T, Keys>', () => {
-  interface Interface {
-    a: string
-    b?: string
-    c?: string
-  }
-
-  type Result = MapPropsToNonOptionalByKey<Interface, 'c'>
-  type A = Result['a']
-  type B = Result['b']
-  type C = Result['c']
-
-  assertType<Equals<A, string>>()
-  assertType<Equals<B, string | undefined>>()
-  assertType<Equals<C, string>>()
-  // @ts-ignore
-  assertType<Equals<
-    Result
-  , {
+describe('MapPropsToNonOptionalByKey<T, Keys>', () => {
+  test('Optional<T>', () => {
+    interface Interface {
       a: string
-      b?: string | undefined
-      c: string
+      b?: string
+      c?: string
     }
-  >>()
+
+    type Result = MapPropsToNonOptionalByKey<Interface, 'c'>
+    type A = Result['a']
+    type B = Result['b']
+    type C = Result['c']
+
+    assertType<Equals<A, string>>()
+    assertType<Equals<B, string | undefined>>()
+    assertType<Equals<C, string>>()
+    // @ts-ignore
+    assertType<Equals<
+      Result
+    , {
+        a: string
+        b?: string | undefined
+        c: string
+      }
+    >>()
+  })
+
+  test('T | undefined', () => {
+    interface Interface {
+      a: string
+      b?: string
+      c: string | undefined
+    }
+
+    type Result = MapPropsToNonOptionalByKey<Interface, 'c'>
+    type A = Result['a']
+    type B = Result['b']
+    type C = Result['c']
+
+    assertType<Equals<A, string>>()
+    assertType<Equals<B, string | undefined>>()
+    assertType<Equals<C, string | undefined>>()
+    // @ts-ignore
+    assertType<Equals<
+      Result
+    , {
+        a: string
+        b?: string | undefined
+        c: string | undefined
+      }
+    >>()
+  })
 })
